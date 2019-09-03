64db88e77a714f87fc16f43262634a003150317a
Jeff-Anthony <jeff.llanos@tecsup.edu.pe>
2019-09-03 17:41:10 -0500

    Master

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -1,16 +1,7 @@
 package com.tecsup.lab01.controller;
-import com.tecsup.lab01.model.*;
 
-<<<<<<< HEAD
 
 public class UserController {
 
 
-=======
-public class UserController extends User {
-
-	String Nombres = "Jeff";
-	String Apellidos = "Llanos Orihuela";
-	int edad = 21;
->>>>>>> JeffLlanos
 }
--- src/main/java/com/tecsup/lab01/model/User.java
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -2,13 +2,4 @@ package com.tecsup.lab01.model;
 
 public class User {
 
-<<<<<<< HEAD
-
-=======
-	String Nombres;
-	String Apellidos;
-	int edad;
-			
->>>>>>> JeffLlanos
-	
 }
