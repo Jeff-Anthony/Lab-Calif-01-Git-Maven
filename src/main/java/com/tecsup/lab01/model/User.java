bd5cd5c6b014e02e52391cd703d180d49b217e6b
Jeff-Anthony <jeff.llanos@tecsup.edu.pe>
2019-09-03 17:14:08 -0500

    JeffLlanos Practica1

--- src/main/java/com/tecsup/lab01/model/User.java
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -2,4 +2,9 @@ package com.tecsup.lab01.model;
 
 public class User {
 
+	String Nombres;
+	String Apellidos;
+	int edad;
+			
+	
 }
--- src/main/java/com/tecsup/lab01/view/Userview.java
+++ src/main/java/com/tecsup/lab01/view/Userview.java
@@ -1,5 +1,13 @@
 package com.tecsup.lab01.view;
+import com.tecsup.lab01.controller.*;
 
 public class Userview {
+	
+	
+    public static void main( String[] args )
+    {
 
+	System.out.println( "Jeff Llanos Orihuela");
+    
+    }
 }
